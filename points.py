e16b87fcbd0af3cc5f2104ced191a737201bc86b
cchsuper <cchsuper@gmail.com>
2020-03-14 17:04:27 -0400

    Fixed tie point assignment

--- points.py
+++ points.py
@@ -41,7 +41,7 @@ with open("./csv/"+os.listdir("./csv/")[-1]) as f:
             points[row[0]] = 0
 
 # Convert times to centiseconds
-for row in data: 
+for row in data:
     for j,time in enumerate(row):
         row[j] = getRaw(time)
 
@@ -57,17 +57,33 @@ for j,row in enumerate(data):
 # Assign Points
 for i,level in enumerate(data2):
     #print("level:"+str(i))
-    reverseSort = [10, 11, 12, 13, 22, 23, 26, 39, 40, 54, 55, 65, 66, 68, 69, 70, 83, 92, 95, 100, 101, 103, 104, 105]
+    reverseSort = [10,11,12,13,22,23,26,39,40,54,55,65,
+                   66,68,69,70,83,92,95,100,101,103,104,105]
     if i in reverseSort:
         sortedLevel = sorted(level,key=lambda x: x[1])
     else:
         sortedLevel = sorted(level,key=lambda x: x[1], reverse=True)
 
-    pointsToAdd = 1
+    pointsToAdd = 0
+    tied=[]
+    prev =("placeholder",-1)
     for j,entry in enumerate(sortedLevel):
         if entry[1] > 0:
-            points[entry[0]] += pointsToAdd
+
+            if entry[1] == prev[1]:
+                pass
+            else:
+                for entry2 in tied:
+                    points[entry2[0]] += pointsToAdd
+                tied = []
+
+            tied.append(entry)
             pointsToAdd+=1
+            prev = entry
+
+    if len(tied)>0:
+        for entry in tied:
+            points[entry[0]] += pointsToAdd
 
 # Display Sorted Ranking
 ranking = sorted(points, key=lambda x: points[x], reverse=True)
